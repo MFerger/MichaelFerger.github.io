99879da3a2b62407b2a4c442f536ce79cb4dd5c4
Michael Ferger <mferger92@gmail.com>
2016-03-01 19:34:39 -0700

    first commit

---
+++ preload.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('#killed').hide();
+
+  $('#play').on('click', function(event) {
+    if ($('input[name=input]').val().length === 0) {
+      alert("Please type in your name.");
+      $("#userNameInput").focus();
+      event.preventDefault();
+    } else {
+      $('#testing').css({
+        'display': 'none'
+      });
+      var value = $('input:text[name=input]').val();
+      $('#userName').text(value)
+      $('#userNameInput').val("");
+    }
+  });
+
+  $('#scores').on('click', function() {
+    $('.killed').slideToggle('slow', function() {})
+  });
+
+})
